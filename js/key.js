f95abe4d34230690c7f2e96bbccda453f2984a7e
Edgar Santana <edgarfranciscos@gmail.com>
2023-03-29 01:00:09 -0600

    fixing new key

--- js/key.js
+++ js/key.js
@@ -7,11 +7,20 @@
  * quedar igual que en tu
  * servidor. */
 // @ts-ignore
-firebase.initializeApp({
+/* firebase.initializeApp({
   apiKey: "AIzaSyC2JZqtXQB7GDELzx9EGDwXvPAzGow_M-Q",
   authDomain: "ammper-f7ca6.firebaseapp.com",
   projectId: "ammper-f7ca6",
   storageBucket: "ammper-f7ca6.appspot.com",
   messagingSenderId: "1085945531463",
   appId: "1:1085945531463:web:4090d8ba00215c58263562"
+}); */
+firebase.initializeApp({
+  apiKey: "AIzaSyAV5DOMUBmGU2m-XPK6idGYDcPTidcMY3U",
+    authDomain: "edgarsantana-4845e.firebaseapp.com",
+    projectId: "edgarsantana-4845e",
+    storageBucket: "edgarsantana-4845e.appspot.com",
+    messagingSenderId: "1003184181306",
+    appId: "1:1003184181306:web:1887f8c6e20931361c2503",
+    measurementId: "G-48RB6H552B"
 });
